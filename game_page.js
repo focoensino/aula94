8a8ffc23f4e6b66a9e040446014dff756f2fd59e
Sérgio Tafine <focoensinoonline@gmail.com>
2023-12-22 19:12:22 -0300

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -50,44 +50,6 @@ answerTurn = "player2";
 
 function check()
 {
-	getAnswer = document.getElementById("inputCheckBox").value;
-	answer = getAnswer.toLowerCase();
-	console.log("resposta em caixa baixa - " + answer);
-	if(answer == word)	
-	{
-		if(answerTurn == "player1")
-		{
-			player1Score = player1Score +1;
-		    document.getElementById("player1Score").innerHTML = player1Score;
-		}
-		else 
-		{
-			player2Score = player2Score +1;
-		    document.getElementById("player2Score").innerHTML = player2Score;
-		}
-	}
-	if(questionTurn == "player1")
-	{
-		questionTurn = "player2"
-		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player2Name ;
-	}
-	else 
-	{
-		questionTurn = "player1"
-		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player1Name ;
-	}
-
-	if(answerTurn == "player1")
-	{
-		answerTurn = "player2"
-		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player2Name ;
-	}
-	else 
-	{
-		answerTurn = "player1"
-		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player1Name ;
-	}
-
-    document.getElementById("output").innerHTML = "";
+	
 }
 
